ebe3df551b2352382d95727b901c9d9c69604dd1
yxd <345050448@qq.com>
2020-04-13 21:26:43 +0800

    添加了text2

---
+++ wyydemo/src/components/text2.vue
@@ -0,0 +1,22 @@
+<template>
+<div class='box'>
+    <h3>测试2</h3>
+</div>
+</template>
+<script>
+export default {
+props: ['String'],
+data() {
+return {
+};
+},
+methods: {
+},
+components:{
+},
+mounted() {},
+activated() {},
+};
+</script>
+<style lang='less' scoped>
+</style>
